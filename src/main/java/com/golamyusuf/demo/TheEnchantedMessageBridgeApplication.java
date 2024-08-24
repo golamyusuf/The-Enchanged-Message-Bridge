a97344dcf119d8d3625f9b4fd81c0a5fcfc19339
Golam Yusuf <yusuf_siffo@yahoo.com>
2024-08-24 12:19:24 +0600

    Initial Commits

---
+++ src/main/java/com/golamyusuf/demo/TheEnchantedMessageBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.golamyusuf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEnchantedMessageBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheEnchantedMessageBridgeApplication.class, args);
+	}
+
+}
