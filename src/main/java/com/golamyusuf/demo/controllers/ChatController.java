689f44846b3b255f8725ebd1b876d8f8987daf73
Golam Yusuf <yusuf_siffo@yahoo.com>
2024-08-25 23:21:18 +0600

    Issue fixed regarding socket connections and message receiving.

---
+++ src/main/java/com/golamyusuf/demo/controllers/ChatController.java
@@ -0,0 +1,30 @@
+package com.golamyusuf.demo.controllers;
+
+import com.golamyusuf.demo.entities.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    @MessageMapping("/api/v1/message/send") // Maps messages sent to /app/send
+    @SendTo("/topic/group") // Broadcasts messages to /topic/messages
+    public Message sendMessage(Message message) {
+        System.out.println(" 18 ChatController message "+message.toString());
+        return message;
+    }
+
+    @MessageMapping("/newUser")
+    @SendTo("/topic/group")
+    public Message addUser(@Payload Message message,
+                           SimpMessageHeaderAccessor headerAccessor) {
+        // Add user in web socket session
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
+}
